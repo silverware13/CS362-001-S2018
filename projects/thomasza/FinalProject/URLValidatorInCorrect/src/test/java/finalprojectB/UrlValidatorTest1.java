ef429bd2cda3d06233f51b22494f8fa105472fd4
silverware13 <thomasza@oregonstate.edu>
2018-06-05 12:39:57 -0700

    added some basic test

--- projects/thomasza/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest1.java
+++ projects/thomasza/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest1.java
@@ -11,40 +11,40 @@ import junit.framework.TestCase;
 
 
 
-public class UrlValidatorTest extends TestCase {
+public class UrlValidatorTest1 extends TestCase {
 
 
-   public UrlValidatorTest(String testName) {
+   public UrlValidatorTest1(String testName) {
       super(testName);
    }
 
-   
-   
+
+
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+//You can use this function to implement your manual testing
+
    }
-   
-   
+
+
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 //You can use this function to implement your First Partition testing
 
    }
-   
+
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+		 //You can use this function to implement your Second Partition testing
 
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
+   //You need to create more test cases for your Partitions if you need to
+
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
 
    }
-   
+
 
 
 }
--- projects/thomasza/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest2.java
+++ projects/thomasza/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest2.java
@@ -1,50 +1,67 @@
 
 package finalprojectB;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.util.Arrays;
+import static org.junit.Assert.*;
+import java.io.*;
+
 import junit.framework.TestCase;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
 
+public class UrlValidatorTest2 extends TestCase {
 
 
-
-
-public class UrlValidatorTest extends TestCase {
-
-
-   public UrlValidatorTest(String testName) {
+   public UrlValidatorTest2(String testName) {
       super(testName);
    }
 
-   
-   
-   public void testManualTest()
+   @Test(timeout = 4000)
+   public void testManualTest_01()
    {
-//You can use this function to implement your manual testing	   
-	   
+       UrlValidator urlVal = new UrlValidator(null, null, 1 << 0);
+       String url = "http://www.google.com";
+       assertTrue("This url should be valid", urlVal.isValid(url));
    }
-   
-   
+
+   @Test(timeout = 4000)
+   public void testManualTest_02()
+   {
+       UrlValidator urlVal = new UrlValidator(null, null, 1 << 0);
+       String url = "ftp://www.google.com";
+       assertTrue("This url should be valid", urlVal.isValid(url));
+   }
+
+   @Test(timeout = 4000)
+   public void testManualTest_03()
+   {
+       UrlValidator urlVal = new UrlValidator(null, null, 1 << 0);
+       String url = "httpw://www.google.com";
+       assertFalse("This url should NOT be valid", urlVal.isValid(url));
+   }
+
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 //You can use this function to implement your First Partition testing
 
    }
-   
+
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+		 //You can use this function to implement your Second Partition testing
 
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
+   //You need to create more test cases for your Partitions if you need to
+
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
 
    }
-   
-
-
 }
