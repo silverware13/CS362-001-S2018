1264866919fe255b86cb3c1bddbdb713dd0305e2
silverware13 <thomasza@oregonstate.edu>
2018-06-06 13:20:27 -0700

    start of program test. Has some issues with valid tests

--- projects/thomasza/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/thomasza/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -1,5 +1,6 @@
 package finalprojectB;
 
+import java.util.Random;
 import java.util.Arrays;
 import static org.junit.Assert.*;
 
@@ -10,7 +11,6 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 public class UrlValidatorTest extends TestCase {
-
 
    public UrlValidatorTest(String testName) {
    	super(testName);
@@ -24,9 +24,9 @@ public class UrlValidatorTest extends TestCase {
        String assertMessage = String.format("This url should be valid: %s", url);
        assertTrue(assertMessage, urlVal.isValid(url));
    }
-/*
+
    //test valid ftp address
-   public void testManualTest_02() //FINDS BUGS (EXCEPTION)
+   public void testManualTest_02()
    {
        UrlValidator urlVal = new UrlValidator(null);
        String url = "ftp://www.website.com";
@@ -42,7 +42,7 @@ public class UrlValidatorTest extends TestCase {
        String assertMessage = String.format("This url should be valid: %s", url);
        assertTrue(assertMessage, urlVal.isValid(url));
    }
-*/
+
    //test invalid empty address
    public void testManualTestInvalid_01()
    {
@@ -74,18 +74,23 @@ public class UrlValidatorTest extends TestCase {
    public void testYourFirstPartition()
    {
        UrlValidator urlVal = new UrlValidator(null);
+
        String url = "http://www.website.com"; // medium length valid scheme
        String assertMessage = String.format("This url should be valid: %s", url);
        assertTrue(assertMessage, urlVal.isValid(url));
+
        url = "https://www.website.com"; // longest valid scheme
        assertMessage = String.format("This url should be valid: %s", url);
        assertTrue(assertMessage, urlVal.isValid(url));
+
        url = "ftp://www.website.com"; // shortest valid scheme
        assertMessage = String.format("This url should be valid: %s", url);
        assertTrue(assertMessage, urlVal.isValid(url));
+
        url = "httpss://www.website.com"; // just too long invalid scheme
        assertMessage = String.format("This url should NOT be valid: %s", url);
        assertFalse(assertMessage, urlVal.isValid(url));
+
        url = "ft://www.website.com"; // just too short invalid scheme
        assertMessage = String.format("This url should NOT be valid: %s", url);
        assertFalse(assertMessage, urlVal.isValid(url));
@@ -95,26 +100,76 @@ public class UrlValidatorTest extends TestCase {
    public void testYourSecondPartition()
    {
       UrlValidator urlVal = new UrlValidator(null);
+
       String url = "http://127.90.53.34"; // medium valid authority
       String assertMessage = String.format("This url should be valid: %s", url);
       assertTrue(assertMessage, urlVal.isValid(url));
+
       url = "https://255.255.255.255"; // largest valid authority
       assertMessage = String.format("This url should be valid: %s", url);
       assertTrue(assertMessage, urlVal.isValid(url));
+
       url = "https://0.0.0.0"; // smallest valid authority
       assertMessage = String.format("This url should be valid: %s", url);
       assertTrue(assertMessage, urlVal.isValid(url));
+
       url = "https://255.255.255.256"; // just too large invalid authority
       assertMessage = String.format("This url should NOT be valid: %s", url);
       assertFalse(assertMessage, urlVal.isValid(url));
+
       url = "https://0.0.0.-1"; // just too small invalid authority
       assertMessage = String.format("This url should NOT be valid: %s", url);
       assertFalse(assertMessage, urlVal.isValid(url));
    }
 
+   //test that splits urls into valid or invalid sections
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
+      String url;
+      Random rand = new Random();
+      UrlValidator urlVal = new UrlValidator(null);
 
+      //valid schemes
+      String[] schemes = {"http://", "ftp://", "https://"};
+
+      //invalid schemes
+      String[] invalidSchemes = {"httpss://", "http//", "http:\\"};
+
+      //valid authorities
+      String[] authoritys = {"www.website.com", "123.50.43.11",
+                                "255.255.255.255"};
+      //invalid authorities
+      String[] invalidAuthoritys = {"300.55.12.33", "website", ""};
+
+      //test these custom urls against the method isValid
+      for(int i = 1; i < 1000; i++){
+          //find what section will have a bad input if any
+          int badSection = (int )(rand.nextInt(4));
+          int schemeVal = (int )(rand.nextInt(3));
+          int authorityVal = (int )(rand.nextInt(3));
+
+          //get the correct sections and put them together
+          switch(badSection){
+              case 1: url = invalidSchemes[schemeVal] + authoritys[authorityVal];
+                      break;
+              case 2: url = schemes[schemeVal] + invalidAuthoritys[authorityVal];
+                      break;
+              case 3: url = invalidSchemes[schemeVal] + invalidAuthoritys[authorityVal];
+                      break;
+              default: url = schemes[schemeVal] + authoritys[authorityVal];
+                      break;
+          }
+
+          //check the validity of the url in the isValid method
+          if(badSection > 0){
+              String assertMessage = String.format("This url should NOT be valid: %s", url);
+              assertFalse(assertMessage, urlVal.isValid(url));
+              System.out.printf("Invalid: %s\n", url);
+          } else {
+              String assertMessage = String.format("This url should be valid: %s", url);
+              assertFalse(assertMessage, urlVal.isValid(url));
+              System.out.printf("Valid: %s\n", url);
+          }
+      }
    }
 }
